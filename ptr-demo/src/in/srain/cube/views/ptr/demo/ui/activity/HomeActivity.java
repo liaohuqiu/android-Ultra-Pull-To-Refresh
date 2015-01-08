2d72f894a8fc78b2d6ecc44b7858bf5a9eeeb0ac
liaohuqiu <liaohuqiu@gmail.com>
2015-01-08 10:29:51 +0800

    update for cube-sdk

--- ptr-demo/src/in/srain/cube/views/ptr/demo/ui/activity/HomeActivity.java
+++ ptr-demo/src/in/srain/cube/views/ptr/demo/ui/activity/HomeActivity.java
@@ -1,11 +1,11 @@
 package in.srain.cube.views.ptr.demo.ui.activity;
 
 import android.os.Bundle;
-import in.srain.cube.mints.base.DemoBaseActivity;
+import in.srain.cube.mints.base.MintsBaseActivity;
 import in.srain.cube.views.ptr.demo.R;
 import in.srain.cube.views.ptr.demo.ui.HomeFragment;
 
-public class HomeActivity extends DemoBaseActivity {
+public class HomeActivity extends MintsBaseActivity {
 
     @Override
     protected void onCreate(Bundle bundle) {
