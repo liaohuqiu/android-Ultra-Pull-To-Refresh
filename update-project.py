9fb344f04bfa55688134439d8da5f2ecd8ad4370
liaohuqiu <liaohuqiu@gmail.com>
2015-03-13 14:48:52 +0800

    release 1.0.8

--- update-project.py
+++ update-project.py
@@ -1,6 +1,6 @@
 import os
 vars = {
-        'ptr_lib_version': '1.0.8.3-SNAPSHOT',
+        'ptr_lib_version': '1.0.8',
         'ptr_lib_stable_version': '1.0.7',
         'cube_sdk_version': '1.0.44.14-SNAPSHOT'
         }
