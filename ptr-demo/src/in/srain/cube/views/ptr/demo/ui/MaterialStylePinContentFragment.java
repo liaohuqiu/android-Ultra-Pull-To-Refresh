8fd690787cfa1a7e3723b42eae7ce20cd7c6d1a5
liaohuqiu <liaohuqiu@gmail.com>
2015-04-06 18:08:36 +0800

    fix #72, fix #69

---
+++ ptr-demo/src/in/srain/cube/views/ptr/demo/ui/MaterialStylePinContentFragment.java
@@ -0,0 +1,16 @@
+package in.srain.cube.views.ptr.demo.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MaterialStylePinContentFragment extends MaterialStyleFragment {
+
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.createView(inflater, container, savedInstanceState);
+        mPtrFrameLayout.setPinContent(true);
+        return view;
+    }
+}
