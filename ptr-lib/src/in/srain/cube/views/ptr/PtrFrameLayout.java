754151ddf4aa933f660edc3f76358e2488593e75
yanlu <caoqinmiao@gmail.com>
2016-04-16 09:53:53 +0800

    1. upgrade gradle 2. when horizontal move, turn down the threshold distance.

--- ptr-lib/src/in/srain/cube/views/ptr/PtrFrameLayout.java
+++ ptr-lib/src/in/srain/cube/views/ptr/PtrFrameLayout.java
@@ -308,7 +308,7 @@ public class PtrFrameLayout extends ViewGroup {
                 float offsetX = mPtrIndicator.getOffsetX();
                 float offsetY = mPtrIndicator.getOffsetY();
 
-                if (mDisableWhenHorizontalMove && !mPreventForHorizontal && (Math.abs(offsetX) > mPagingTouchSlop && Math.abs(offsetX) > Math.abs(offsetY))) {
+                if (mDisableWhenHorizontalMove && !mPreventForHorizontal && (Math.abs(offsetX) > 0 && (Math.abs(offsetX) - Math.abs(offsetY) > 0.4))) {
                     if (mPtrIndicator.isInStartPosition()) {
                         mPreventForHorizontal = true;
                     }
